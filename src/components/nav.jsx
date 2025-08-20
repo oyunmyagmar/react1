15ca9df921e3b1b9d00f3b19318a0a1a407cf479
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-20 11:11:20 +0800

    last ver

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,7 +2,7 @@ export const Nav = (props) => {
   return (
     <div className="flex place-content-between py-4 px-12">
       <h5 className="font-bold text-lg">{props.name}</h5>
-      <div className="flex gap-6 text-sm">
+      <div className="flex gap-6 text-[12px]">
         <a href="https://www.google.com/">{props.home}</a>
         <a href="https://www.google.com/">{props.about}</a>
         <a href="https://www.google.com/">{props.contact}</a>
