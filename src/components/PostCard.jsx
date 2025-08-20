7c88c156ebcb0d8763f0813ea809f35b4c99991d
oyunmyagmar <25LP4018@nest.edu.mn>
2025-08-20 11:07:58 +0800

    finished card

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -4,7 +4,9 @@ export const PostCard = (props) => {
       <img className="w-full h-[220px] pt-5" src={props.image} alt=""></img>
       <div className="p-5">
         <p className="text-[12px]">{props.date}</p>
-        <h3 className="mt-2 text-lg font-semibold">{props.title}</h3>
+        <h3 className="mt-2 text-lg leading-none font-semibold">
+          {props.title}
+        </h3>
         <p className="mt-3.5 text-sm">{props.description}</p>
         <button className="mt-3 bg-blue-500 w-full text-white rounded-sm text-[12px] py-1">
           Read More
